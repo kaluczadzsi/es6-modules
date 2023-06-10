140bc381d845aeab99263f4a6dc540799a586dd3
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-06-10 15:53:10 +0200

    I learned how ES6 modules work

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import User from '/user.js';
+import User, { printName as printUserName, printAge } from '/user.js';
 
 const user = new User('Bob', 24);
 console.log(user);
+
+printUserName(user);
+printAge(user);
